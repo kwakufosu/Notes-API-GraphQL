5d22f7221b3283d2312e605bbf0ac0aea05f155c
kwaku fosu <kwakuboachiefosu@gmail.com>
2022-11-24 14:36:03 +0000

    Completed user queries and mutations

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,22 +4,20 @@ require('dotenv').config();
 
 const auth = async (token) => {
   try {
-    const tokenOnly = token.split(' ')[1];
+    const tokenOnly = token.split(' ')[1].replace(/\"/g, '');
 
-    const decoded = await jwt.verify(
-      tokenOnly.replace(/\"/g, ''),
-      process.env.SECRET
-    );
+    const decoded = await jwt.verify(tokenOnly, process.env.SECRET);
 
     const user = await User.findOne({
       _id: decoded._id,
+      'tokens.token': tokenOnly,
     });
 
     if (!user) {
       throw new Error('Please authenticate');
     }
 
-    return user;
+    return { user, tokenOnly };
   } catch (e) {
     throw new Error('Please authenticate');
   }
--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 const auth = require('../middleware/auth');
-
+const bcrypt = require('bcrypt');
 const resolvers = {
   Query: {
     greeting: () => 'Hello world',
@@ -17,7 +17,7 @@ const resolvers = {
       }
     },
     getCurrentUserDetails: async function (root, _, { token }) {
-      return await auth(token);
+      return ({ user } = await auth(token));
     },
   },
 
@@ -29,14 +29,73 @@ const resolvers = {
         const token = await user.generateAuthToken();
         await user.save();
 
-        return {
-          user,
-          token,
-        };
+        return { user, token };
       } catch (e) {
         console.log(e);
         throw new Error('Unsucccessful');
       }
+    },
+
+    updateCurrentUser: async function (_, { input }, { token }) {
+      const { user } = await auth(token);
+      const userID = user._id.toString();
+
+      if (input.password) {
+        input.password = await bcrypt.hash(input.password, 8);
+      }
+      if (!user && !(userID === input._id)) {
+        return;
+      }
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { ...input },
+        { new: true, runValidators: true }
+      );
+
+      await updatedUser.save();
+
+      return updatedUser;
+    },
+
+    logoutCurrentUser: async function (_, { _id }, { token }) {
+      const { user, tokenOnly } = await auth(token);
+
+      if (!user && !(user._id.toString() === _id)) {
+        return;
+      }
+
+      user.tokens = user.tokens.filter((userToken) => {
+        return userToken.token !== tokenOnly;
+      });
+
+      await user.save();
+
+      return user;
+    },
+
+    logoutAll: async function (_, { _id }, { token }) {
+      const { user } = await auth(token);
+
+      if (!user && !(user._id.toString() === _id)) {
+        return;
+      }
+
+      user.tokens = [];
+
+      await user.save();
+      return user;
+    },
+
+    deleteCurrentUser: async function (_, { _id }, { token }) {
+      const { user } = await auth(token);
+      const deletedUser = user;
+      if (!user && !(user._id.toString() === _id)) {
+        return;
+      }
+
+      user.remove();
+
+      return deletedUser;
     },
   },
 };
